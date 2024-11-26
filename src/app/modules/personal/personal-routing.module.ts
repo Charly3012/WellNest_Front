773628514f5837e6113ed6bcb71a9b71bdab07db
desarrollador3he <desarrollador3he>
2024-11-25 21:57:24 -0600

    adición de metas

--- src/app/modules/personal/personal-routing.module.ts
+++ src/app/modules/personal/personal-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BinnacleComponent } from './pages/binnacle/binnacle.component';
+import { GoalsComponent } from './pages/goals/goals.component';
 
 const routes: Routes = [
   {
     path: 'binnacle',
     component: BinnacleComponent
+  },
+  {
+    path: 'goals', // Nueva ruta
+    component: GoalsComponent, // Componente asociado
   }
 ];
 
--- src/app/modules/personal/personal.module.ts
+++ src/app/modules/personal/personal.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PersonalRoutingModule } from './personal-routing.module';
 import { BinnacleComponent } from './pages/binnacle/binnacle.component';
+import { GoalsComponent } from './pages/goals/goals.component';
 
 
 @NgModule({
   declarations: [
-    BinnacleComponent
+    BinnacleComponent,
+    GoalsComponent
   ],
   imports: [
     CommonModule,
