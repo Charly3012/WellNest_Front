679816e2e062551100e34842870c6cfeb64f1ae1
Elias Gallegos <eliasaerg@gmail.com>
2024-11-30 17:12:28 -0500

    add component State

--- src/app/modules/social/social.module.ts
+++ src/app/modules/social/social.module.ts
@@ -7,6 +7,7 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { ProfileSettingsComponent } from './pages/profile-settings/profile-settings.component';
 import { PostComponent } from './components/post/post.component';
 import { SearchComponent } from './pages/search/search.component';
+import { StateComponent } from './pages/state/state.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { SearchComponent } from './pages/search/search.component';
     SearchComponent,
     ProfileComponent,
     ProfileSettingsComponent,
-    PostComponent
+    PostComponent,
+    StateComponent
   ],
   imports: [
     CommonModule,
