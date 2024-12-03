246f2a648f34477b9863c72420cadaf1979efc9d
Charly3012 <carlocoronadosilva975@gmail.com>
2024-12-03 00:31:18 -0600

    base: Validations at register form

--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -3,6 +3,8 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthServiceService } from '../../auth-service.service';
 import { RegisterData } from '../../models/RegisterData';
+import { error } from 'console';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -13,6 +15,7 @@ export class RegisterComponent implements OnInit {
 
   public registerData: RegisterData = {};
   public rePassword: string = '';
+  private debounceRegister = false; //Control to avoid multiple sends at register post
 
   constructor(
     private authService: AuthServiceService,
@@ -25,28 +28,64 @@ export class RegisterComponent implements OnInit {
   register(form: NgForm){
     console.log('form values', form.value)
 
-    if(!this.checkPassword()){
+    if(this.checkPassword() == 1){
       alert('Las contraseñas no coinciden');
       return;
     }
 
+    if(this.checkPassword() == 2){
+      alert('La contraseña debe ser de almenos 8 caracteres');
+      return;
+    }
+
+    if(!this.checkEmail(this.registerData.email!)){
+      alert('El email introducido no es valido');
+      return
+    }
 
     this.authService.register(this.registerData)
     .subscribe( response => {
       console.log(response);
       alert('Usuario creado exitosamente');
       this.router.navigate(['/auth/login']);
-  
-    })
+    },
+    (errorResponse) => {
+      //Catch errors provided by backend
+      const errorList : any[] = errorResponse.error.Errors;
+      errorList.forEach(element => {
+        alert('Error en el campo ' + element.Field + ', ' + element.Error);
+      });
+
+    },
+    () => {
+      //Allow clicks after 1 second
+      setTimeout(() => this.debounceRegister = false, 1000);
+    }
+  )
   }
 
-  checkPassword() : boolean{
-    if(this.rePassword == this.registerData.password){
-      return true;
+  //Validations in register form
+  checkPassword() : number {
+    if(this.registerData.password!.length < 8){
+      //Password is not al least 8 characters length
+      return 2;
     }
-    return false;
+
+    if(this.rePassword != this.registerData.password){
+      //password and re-password are differents
+      return 1;
+    }
+    return 0;
   }
-  
+
+  checkEmail(email: string): boolean {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return emailRegex.test(email);
+  }
+
+  //End validations section
+
+
   goToLogin() {
     this.router.navigate(['/auth/login']);
   }
